2bd4a2e2ab75ebff6ab0917aeac3dcd0f3a613e9
max <obregonjefferson2@gmail.com>
2025-02-20 22:19:16 -0500

    footer change design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,18 +7,18 @@ function Footer() {
         <>
             <div className='bg-gray-900 flex justify-center items-center h-20 text-white gap-5'>
                 <p> 2025 Jeff. All rights reserved.</p>
-                    <div className="flex justify-center items-center gap-3">
-                        <Link href="https://www.tiktok.com/@devjef" target="_blank" rel="noopener noreferrer">
-                           <Image src="/tiktok.svg" alt="GitHub" width="60" height="60" />
-                        </Link>
-                    </div>
+                <div className="flex justify-center items-center gap-3">
+                    <Link href="https://www.tiktok.com/@devjef" target="_blank" rel="noopener noreferrer">
+                        <Image src="/tiktok.svg" alt="GitHub" width="60" height="60" />
+                    </Link>
+                </div>
 
-                    <div className="flex justify-center items-center gap-3">
-                        <Link href="https://www.linkedin.com/in/jefferson-obregon-mejia-2245842b0/" target="_blank" rel="noopener noreferrer">
-                            <Image src="/linkeding.svg" alt="LinkedIn" width="50" height="50" />
-                        </Link>
-                    </div>
-                
+                <div className="flex justify-center items-center gap-3">
+                    <Link href="https://www.linkedin.com/in/jefferson-obregon-mejia-2245842b0/" target="_blank" rel="noopener noreferrer">
+                        <Image src="/linkeding.svg" alt="LinkedIn" width="50" height="50" />
+                    </Link>
+                </div>
+
 
             </div>
         </>
